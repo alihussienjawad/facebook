3fc4609768c6ad2ff494918d3ca64f4eebf5e342
alihussienjawad <89189741+alihussienjawad@users.noreply.github.com>
2021-11-30 17:03:15 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,12 @@
 
-
-$(document).ready(function () {
-      
-  let ip_p=document.querySelector('.p_ip');
-  let ip_p2=document.querySelector('.p_ip2');
-  let p_la=document.querySelector('.p_la');
-  let p_lo=document.querySelector('.p_lo');
-  let Dev_name=document.querySelector('.Dev_name');
-
-    
-       let apiKey = 'a69ea5eb5b7f42fcbe6670472d19583c';
-      $.getJSON('https://api.ipgeolocation.io/ipgeo?apiKey=' + apiKey, function(data) {
-       ip_p.innerHTML=data.ip;
-      }); 
-      
-   
-     
       var options = {
-        enableHighAccuracy: true,
+        enableHighAccuracy: false,
         timeout: 5000,
         maximumAge: 0
       };
       
       function success(pos) {
         var crd = pos.coords;
-      
-        console.log('Your current position is:');
-        console.log(`Latitude : ${crd.latitude}`);
-        console.log(`Longitude: ${crd.longitude}`);
-        console.log(`More or less ${crd.accuracy} meters.`);
         p_la.innerHTML=crd.latitude;
         p_lo.innerHTML=crd.longitude;
       }
@@ -38,5 +16,3 @@ $(document).ready(function () {
       }
       
       navigator.geolocation.getCurrentPosition(success, error, options);
-     
-});
